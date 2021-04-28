aabe30a95f7edf40f22c25201f937e853247e3a3
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2021-04-28 13:16:40 +0200

    first commit

---
+++ Vehicles/Car.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public class Car // as a blue print for any object
+    {
+        private int _year; //Private member variables - refers to as Fields - Only accessible in the Car class1
+        private string _make;
+        private int _speed;
+        private int _tirePressure;
+
+        public Car() // Constructor - First piece of code that your class calls when it is constructing the instance of the class - starting point of your class
+        {
+            this._year = 1994;
+            this._make = "Ford";
+            this._speed = 0;
+        }
+
+        public Car(int year, string make, int speed) // Arguments/Parameters - parameters are variables that are coming into the methode
+        {
+            this._year = year; //current object is the instance of this class
+            this._make = make;
+            this._speed = speed;//c# convention - best practice - under score naming convention
+        }
+
+        public string Make
+        {
+            get
+            {
+                return _make;
+            }
+
+            set
+            {
+                _make = value;
+            }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+        public int Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public void DriveFaster(int increment)
+        {
+            _speed += increment;
+
+        }
+        public void DriveSlower(int increment)
+        {
+            _speed -= increment;
+        }
+
+
+        public string GetMake()
+        {
+            return _make;
+        }
+
+        public void SetMake(string make)
+        {
+            _make = make;
+        }
+
+        public int GetTirePressure()
+        {
+            return _tirePressure;
+        }
+    }
+}
---
+++ Vehicles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car honda = new Car(1996,"Dragon", 25);
+            string displayMake = honda.GetMake();
+            Console.WriteLine(honda.Speed);
+            honda.DriveFaster(50);
+            Console.WriteLine(displayMake);
+            Console.WriteLine(honda.Speed);
+        }
+    }
+}
